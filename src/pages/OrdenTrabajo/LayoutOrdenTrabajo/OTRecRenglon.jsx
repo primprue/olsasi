857523b250f6bc3272d395c49e7b11f0c870fa2c
Sandra <sperezk@yahoo.com.ar>
2024-04-15 08:48:32 -0300

    OT datos de renglones elegidos

--- src/pages/OrdenTrabajo/LayoutOrdenTrabajo/OTRecRenglon.jsx
+++ src/pages/OrdenTrabajo/LayoutOrdenTrabajo/OTRecRenglon.jsx
@@ -5,107 +5,9 @@ import { useContext } from "react";
 import OrdTrabajo from "../../../context/OrdTrabajo.jsx";
 export default function OTRecRenglon() {
 	const { otdatos, setOTdatos } = useContext(OrdTrabajo);
+	const [datosppa, setDatosPpa] = useState([]);
 
-	console.log(
-		"otdatos en otdatosOTO  renglonespresup: ",
-		otdatos.renglonespresup
-	);
-	console.log(
-		"otdatos en otdatosOTO  renglonespresup[0][0]: ",
-		otdatos.renglonespresup[0][0].idPresupRenglon
-	);
-
-	// async function initialFetch() {
-	// 	//	columnsFetch();
-	// 	dataOTO();
-	// }
-	const informacionPresupRenglonParamInt = [];
-	const informacion = [];
-	const recorredatos = () => {
-		// Recorre el array de datos y extrae la información de los campos deseados
-		otdatos.renglonespresup.forEach((nivel1) => {
-			nivel1.forEach((nivel2) => {
-				const {
-					id,
-					PresupRenglonNroPresup,
-					PresupRenglonImpItem,
-					PresupRenglonImpUnit,
-					PresupRenglonDesc,
-					PresupRenglonParamInt,
-				} = nivel2;
-				// Aquí puedes hacer lo que quieras con la información extraída, por ejemplo, almacenarla en un nuevo array
-				// informacion.push({
-				// 	id,
-				// 	numeroPresupuesto: PresupRenglonNroPresup,
-				// 	cantidad: PresupRenglonCant,
-				// 	descripcion: PresupRenglonDesc,
-				// });
-				if (PresupRenglonParamInt) {
-					// Aquí puedes hacer lo que quieras con la información extraída
-					informacionPresupRenglonParamInt.push(PresupRenglonParamInt);
-				}
-			});
-		});
-
-		// Devuelve el JSX (en este caso, no estamos mostrando nada)
-		return null;
-	};
-	/*
-
-                renglot1[j] =
-                {
-                    "ordtrabitem": j + 1,
-                    "ordtrabcantidad": datotraido[i].PresupRenglonCant,
-                    "ordtrabdescripcion": datotraido[i].PresupRenglonDesc,
-                    "materialselec": '',
-                    "colorselec": '',
-                    "ordtrablargo": datotraido[i].PresupRenglonLargo * 1,
-                    "ordtrabancho": datotraido[i].PresupRenglonAncho * 1,
-                    "ordtrabimpitemsiva": datotraido[i].PresupRenglonImpUnit * 1 / 1.21,
-                    "ordtrabimpitemciva": datotraido[i].PresupRenglonImpUnit * 1,
-                    "ordtrabimpsiva": impsiva,
-                    "ordtrabimpciva": datotraido[i].PresupRenglonImpItem * 1,
-                    "ordtrabmaterial": datoselegidosaux.StkRubroAbr,
-                    "ordtrabparametros": datoselegidosaux,
-                }
-*/
-	useEffect(() => {
-		recorredatos();
-		// setValor("Lista de Precios");
-	}, []); // eslint-disable-line react-hooks/exhaustive-deps
-	/*
-		PresupRenglonAncho
-: 
-5
-PresupRenglonCant
-: 
-1
-PresupRenglonDesc
-: 
-"Lona con ojales reforzados, chicotes y soga en dobladillo en : COB. 840  "
-PresupRenglonImpItem
-: 
-143200
-PresupRenglonImpUnit
-: 
-143200
-PresupRenglonLargo
-: 
-2
-PresupRenglonNroPresup
-: 
-6393
-PresupRenglonParamInt
-: 
-"{\"StkRubroAbr\":\"ST840\",\"minmay\":\"mn\",\"ivasn\":\"CIVA\",\"cantidad\":1,\"veces\":1,\"largo\":\"2\",\"ancho\":\"5\",\"largon\":0,\"anchon\":0,\"tipoconf\":\"cs\",\"tipoojale\":\"hz\",\"drenajesn\":\"cd\",\"detallep\":\"\",\"detaller\":\"\",\"tamfaja\":\"2P\",\"tamcristal\":\"PVC05\",\"altovolado\":20,\"presupojalesc\":20,\"sobrantemarco\":20,\"tipomedeleg\":\"CC\",\"termbordeeleg\":\"SF\",\"anchopared\":0,\"medida\":0,\"alto\":1.1,\"stkrubroabrtbr\":\"\",\"tipomecanismo\":\"Manual\",\"anchocomedero\":\"0.68\",\"lonanuestraafuera\":\"LN\",\"cantbrazos\":0,\"largobrazo\":0,\"voladosd\":\"S\",\"cantHeb\":0,\"cantCarro\":0,\"tipocarro\":\"\",\"tipoheb\":\"\",\"colocacion\":\"\",\"tipoplaca\":\"\",\"cantPlaca\":0,\"tipopresup\":\"CONFECCIONADA\",\"cotdivisa\":0,\"signomonet\":\"$\"}"
-id
-: 
-1
-idPresupRenglon
-: 
-1
-	*/
-	console.log("estoy en fialuno de orden de ptrabajo  ");
+	console.log("otdatos.renglonespresup  ", otdatos.renglonespresup);
 	return (
 		<div>
 			{otdatos.renglonespresup.map((nivel1, index1) => (
@@ -116,11 +18,70 @@ idPresupRenglon
 							<p>Número de Presupuesto: {nivel2.PresupRenglonNroPresup}</p>
 							<p>Cantidad: {nivel2.PresupRenglonCant}</p>
 							<p>Descripción: {nivel2.PresupRenglonDesc}</p>
-							{/* Agrega más campos según tus necesidades */}
+							<p>Importe Unitario: {nivel2.PresupRenglonImpUnit}</p>
+							<p>Importe Item: {nivel2.PresupRenglonImpItem}</p>
+
+							{nivel1.map((item) => {
+								const paramObjeto = JSON.parse(item.PresupRenglonParamInt);
+								return Object.entries(paramObjeto).map(([campo, valor]) => (
+									<li key={campo}>
+										<strong>{campo}:</strong> {valor}
+									</li>
+								));
+							})}
 						</div>
 					))}
 				</div>
 			))}
 		</div>
 	);
+}
+
+/*
+PresupRenglonAncho
+: 
+5
+PresupRenglonCant
+: 
+1
+PresupRenglonDesc
+: 
+"Lona con ojales reforzados, chicotes y soga en dobladillo en : COB. 840  "
+PresupRenglonImpItem
+: 
+572780
+PresupRenglonImpUnit
+: 
+572780
+PresupRenglonLargo
+: 
+8
+PresupRenglonNroPresup
+: 
+6394
+PresupRenglonParamInt
+: 
+"{\"StkRubroAbr\":\"ST840\",\"minmay\":\"mn\",\"ivasn\":\"CIVA\",\"cantidad\":1,\"detallep\":\"\",\"detaller\":\"\",\"largo\":\"8\",\"ancho\":\"5\",\"tipopresup\":\"CONFECCIONADA\",\"cotdivisa\":0,\"signomonet\":\"$\",\"tipoconf\":\"cs\",\"tipoojale\":\"hz\"}"
+id
+: 
+1
+idPresupRenglon
+: 
+1*/
+{
+	/* {otdatos.renglonespresup.map((renglon, index) => (
+	<div key={index}>
+		<h3>Renglón {index + 1}:</h3>
+		{renglon.map((item) => {
+			const paramObjeto = JSON.parse(item.PresupRenglonParamInt);
+			return Object.entries(paramObjeto).map(([campo, valor]) => (
+				<ul>
+					<li key={campo}>
+						<strong>{campo}:</strong> {valor}
+					</li>
+				</ul>
+			));
+		})}
+	</div>
+))} */
 }
