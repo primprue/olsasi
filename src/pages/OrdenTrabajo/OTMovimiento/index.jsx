c8a161bdb232daa7ed9c7f051741a803e4fa547e
Sandra <sperezk@yahoo.com.ar>
2024-09-11 09:56:56 -0300

    prueba 2

--- src/pages/OrdenTrabajo/OTMovimiento/index.jsx
+++ src/pages/OrdenTrabajo/OTMovimiento/index.jsx
@@ -173,7 +173,7 @@ export default function OTMovimiento() {
 			<Button
 				size="large"
 				variant="text"
-				style={{ color: yellow[600] }}
+				style={{ color: pink[600] }}
 				onClick={() => muestraOT(params)}
 				startIcon={<PreviewTwoToneIcon />}
 			/>
